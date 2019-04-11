868f5bd79b190b447020f781102c99be3382e51a
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-04-11 12:06:42 +0530

    Postiveornegative.java

---
+++ Positiveornegative.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class Postiveornegative
+{
+public static void main(String args[])
+{
+int x;
+Scanner s1=new Scanner(System.in);
+System.out.println("enter the number");
+x=s1.nextInt();
+if(x>o)
+{
+System.out.println("the given number is postive");
+}
+else if(x<0)
+{
+System.out.println("the given number is negative");
+}
+else
+{
+System.out.println("the given number is zero");
+}
+}
+}
