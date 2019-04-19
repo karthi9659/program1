2afc01f688e4902effa4221579eea47c09c6f53e
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-04-19 11:23:41 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+public class Hello
+{
+  public static void main(String args[])throws Exception
+  {
+    BufferedReader br=new BufferedReader(InputStreamReader(System.in));
+    String line=br.readLine();
+    int N=integer.parseInt(line);
+    for(int i=0;i<n;i++)
+    {
+      Sydtem.out.println("Hello");
+    }
+  }  
+}  
