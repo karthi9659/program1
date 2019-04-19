206e417f9b0fcfc99f899be2e6b2d93477477399
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-04-19 17:14:40 +0530

    Create CountofDigits.java

---
+++ CountofDigits.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+public class CountofDigits
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int count=0;
+    System.out.println("Enter a number:");
+    int num=sc.nextInt();
+    while(num!=0)
+    {
+      num=num/10;
+      count++;
+    }
+    System.out.println("Number of Digits in the entered integer are:"+count);
+  }
+}  
