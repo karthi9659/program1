52d1de6fa13b55b14ca1837b45408bf0ac10cd99
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-04-20 10:31:06 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Factorial
+{
+  public static void main(String args[])
+  {
+    int i,fact=1;
+    int number;
+    Scanner s=new Scanner(System.in);
+    number=s.nextInt();
+    for(i=1;i<=number;i++)
+    {
+      fact=fact*i;
+    }
+    System.out.println("Factorial of"+number+ "is" +fact);
+  }
+}  
+    
