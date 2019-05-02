29072f0b67c8def999183680f6f97f1167363553
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-02 16:09:08 +0530

    Create Range1to10.java

---
+++ Range1to10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Range1to10
+{
+  public static void main(String args[])
+  {
+    int i;
+    Scanner sc=new Scanner(System.in);
+    i=sc.nextInt();
+    if((i<=10)&&(i>=1))
+    {
+      System.out.println("Yes");
+    }
+  }
+}  
