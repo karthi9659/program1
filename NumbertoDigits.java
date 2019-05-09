0576115f0442c70363535ffb732a53c2daef721d
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-09 13:14:26 +0530

    Create NumbertoDigits.java

---
+++ NumbertoDigits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class NumbertoDigits
+{
+  public static void main(String args[])
+  {
+    int n;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    String name[]={" ","one","two","three","four","five","six","seven","eight","nine","ten"};
+    System.out.println(name[n]);
+  }
+}  
+    
