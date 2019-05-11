a0f3442a2f8d0ee3132957c39359cdefd2ff2546
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-11 15:33:35 +0530

    Create Sumnatural.java

---
+++ Sumnatural.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Sumnatural
+{
+  public static void main(String args[])
+  {
+    int x.i=1;
+    int sum=0;
+    System.out.println("Enter the value");
+    Scanner s=new Scanner(System.in);
+    x=s.nextInt();
+    while(i<=x)
+    {
+      sum=sum+i;
+      i++;
+    }
+    System.out.println("Sum of"+x+"number is:"+sum);
+  }
+}  
