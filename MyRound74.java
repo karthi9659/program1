0ef297ac8e76f575dbdc7c98001a0005cbedc690
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-12 12:57:08 +0530

    Create MyRound74.java

---
+++ MyRound74.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+class MyRound74
+{
+  public static void main(String args[])
+  {
+    float x=3.2f
+    System.out.println(Math.round(x));
+     float y=2.4f
+    System.out.println(Math.round(y);
+  }
+}  
+  
+  
