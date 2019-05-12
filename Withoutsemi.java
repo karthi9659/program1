bf425e9c24eded23b820b23d724a3d7bf161c94a
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-12 08:32:50 +0530

    Create Withoutsemi.java

---
+++ Withoutsemi.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ class Withoutsemi
+{
+  public static void main(String args[])
+  {
+    int i=0;
+    String str;
+    Scanner s=new Scanner(System.in);
+    str=s.nextLine();
+    if(System.out.printf("")==null){}
+    for(i=1;i<2;System.out.println("Hello World"))
+    {
+      i++;
+    }
+  }
+}  
