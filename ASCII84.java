d40c15e8333b459065f9625d580d3d8886440390
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-13 16:02:27 +0530

    Create ASCII84.java

---
+++ ASCII84.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ASCII84
+{
+  public static void main(String args[])
+  {
+    char ch;
+    Scanner s=new Scanner(System.in);
+    ch=s.next().charAt(0);
+    int ascii=ch;
+    System.out.println(ascii);
+  }
+}  
