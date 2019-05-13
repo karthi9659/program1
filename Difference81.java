7fda3f0024803923863ef256f87934f2bd27243d
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-13 15:10:27 +0530

    Create Difference81.java

---
+++ Difference81.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Difference81
+{
+  public static void main(String args[])
+  {
+    int num1,num2;
+    Scanner s=new Scanner(System.in);
+    num1=s.nextInt();
+    num2=s.nextInt();
+    if(num>num2)
+    {
+      System.out.println(num1-num2);
+    }
+    else
+    {
+      System.out.println(num2-num1);
+    }
+  }
+}  
