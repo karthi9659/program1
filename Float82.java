a3fd8671f35bbc9c59766f0c8c9b333537604e5e
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-13 15:20:00 +0530

    Create Float82.java

---
+++ Float82.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Float83
+{
+  public static void main(String args[])
+  {
+     double a,b;
+     Scanner s=new Scanner(System.in);
+     a=s.nextFloat();
+     b=s.nextFloat();
+     double x=a+b;
+     System.out.println("%.sf"+c);
+  }
+}  
+   
