fb3d5c835f03fcf26ea3ab36542da7c352d326b1
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-14 12:19:44 +0530

    Create SimpleInterest95.java

---
+++ SimpleInterest95.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Simpleinterest95
+{
+  public static void main(String args[])
+  {
+    int a,b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    int sum=(a*b*c)/100;
+    System.out.println(sum);
+  }
+}  
