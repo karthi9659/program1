9fd1d64614da35cf7deb0c0f287c522568f67012
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-14 11:21:54 +0530

    Create Cuboid91.java

---
+++ Cuboid91.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Cuboid91
+{
+  public static void main(String args[])
+  {
+     int l,w,h,vol;
+     Scanner s=new Scanner(System.in);
+     l=s.nextInt();
+     w=s.nextInt();
+     h=s.nextInt();
+     int totalsurarea=2*(l*w+W*h+h*l);
+     System.out.println(totalsurarea);
+     vol=i*w*h;
+     System.out.println(" ");
+     System.out.println(vol);
+  }
+}  
+ 
