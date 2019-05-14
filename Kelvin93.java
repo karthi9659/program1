3e35d13b955d8104f676356d9708cb6a8abc76e6
karthi9659 <49307427+karthi9659@users.noreply.github.com>
2019-05-14 11:47:16 +0530

    Create Kelvin93.java

---
+++ Kelvin93.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Kelvin93
+{
+  public static void main(String args[])
+  {
+    int n;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    float kelvin=n+273.15f;
+    System.out.print(Math.round(kelvin));
+  }
+}  
